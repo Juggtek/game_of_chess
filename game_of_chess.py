55427d7150481f201f5424951892b0a63a88cc7a
Felix <niedermairfelix@gmail.com>
2018-12-24 12:03:29 +0100

    Bishop Capture

--- game_of_chess.py
+++ game_of_chess.py
@@ -138,7 +138,72 @@ class Bishop(Piece):
 
         else:
             print "Invalid Movement!"
-        return
+        return board
+
+    def capture(self, new_col, new_row):
+        next_col = self.col
+        next_row = self.row
+
+        if self.col < new_col and self.row < new_row and \
+           (new_col - self.col) == (new_row - self.row):
+            while next_col != new_col and next_row != new_row:
+                next_col += 1
+                next_row += 1
+                if board[7-next_row][next_col] != "0 " and \
+                     board[7-next_row][next_col][1] != self.colour:
+                    self.movement(next_col, next_row)
+                elif board[7-next_row][next_col] == "0 ":
+                    print "Nothing to capture!"
+                else:
+                    print "Invalid Movement!"
+            self.move_count += 1
+
+        elif self.col < new_col and self.row > new_row and \
+             (new_col - self.col) == (self.row - new_row):
+            while next_col != new_col and next_row != new_row:
+                next_col += 1
+                next_row -= 1
+                if board[7-next_row][next_col] != "0 " and \
+                     board[7-next_row][next_col][1] != self.colour:
+                    self.movement(next_col, next_row)
+                elif board[7-next_row][next_col] == "0 ":
+                    print "Nothing to capture!"
+                else:
+                    print "Invalid Movement!"
+            self.move_count += 1
+
+        elif self.col > new_col and self.row < new_row and \
+             (self.col - new_col) == (new_row - self.row):
+            while next_col != new_col and next_row != new_row:
+                next_col -= 1
+                next_row += 1
+                if board[7-next_row][next_col] != "0 " and \
+                     board[7-next_row][next_col][1] != self.colour:
+                    self.movement(next_col, next_row)
+                elif board[7-next_row][next_col] == "0 ":
+                    print "Nothing to capture!"
+                else:
+                    print "Invalid Movement!"
+            self.move_count += 1
+
+        elif self.col > new_col and self.row > new_row and \
+             (self.col - new_col) == (self.row - new_row):
+            while next_col != new_col and next_row != new_row:
+                next_col -= 1
+                next_row -= 1
+                if board[7-next_row][next_col] != "0 " and \
+                     board[7-next_row][next_col][1] != self.colour:
+                    self.movement(next_col, next_row)
+                elif board[7-next_row][next_col] == "0 ":
+                    print "Nothing to capture!"
+                else:
+                    print "Invalid Movement!"
+            self.move_count += 1
+
+        else:
+            print "Invalid Movement!"
+        return board
+
 
 class Queen(Piece):
     def move(self, new_col, new_row):
