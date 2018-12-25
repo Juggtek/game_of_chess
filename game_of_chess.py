31290ec4e136b49df04fdf45e6e114748921ca19
Felix <niedermairfelix@gmail.com>
2018-12-25 18:09:08 +0100

    rook movement and capture

--- game_of_chess.py
+++ game_of_chess.py
@@ -6,6 +6,7 @@ from board_pieces.piece import Piece
 from board_pieces.pawn import Pawn
 from board_pieces.knight import Knight
 from board_pieces.bishop import Bishop
+from board_pieces.rook import Rook
 
 board = np.array([["0 "]*8]*8)
 
@@ -21,11 +22,12 @@ class Queen(Piece):
 white_pawns = [None] * 8
 for i in range(0,8):
     white_pawns[i] = Pawn(board, "P", "w",i,1)
-
 white_knight_1 = Knight(board,"K","w",1,0)
 white_knight_2 = Knight(board,"K","w",6,0)
 white_bishop_1 = Bishop(board,"B","w",2,0)
 white_bishop_2 = Bishop(board,"B","w",5,0)
+white_rook_1 = Rook(board,"R","w",0,0)
+white_rook_2 = Rook(board,"R","w",7,0)
 
 black_pawns = [None] * 8
 for i in range(0,8):
@@ -34,17 +36,9 @@ for i in range(0,8):
 
 white_pawns[3].move(3,2)
 
-print board
+print board, "\n"
 white_bishop_1.move(5,3)
-print board
-white_bishop_1.move(4,4)
-print board
-white_bishop_1.move(3,3)
-print board
-white_bishop_1.capture(6,6)
-print board
-white_bishop_1.move(5,7)
-print board
-white_bishop_1.capture(4,6)
+
+
 
 print board
